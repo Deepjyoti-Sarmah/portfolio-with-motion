e6d045f0ca5209e067ce4fad4c75a665a69aa4df
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-07-20 15:55:02 +0530

    linking projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,7 @@ import { Project, projects as defaultProjects } from "@/constants/projects";
 import Image from "next/image";
 import { SectionHeading } from "./section-heading";
 import { motion } from "motion/react";
+import Link from "next/link";
 
 const MotionCard = motion.create(_Card, {
   forwardMotionProps: true,
@@ -22,36 +23,44 @@ export function Projects({
       </SectionHeading>
       <div className="grid grid-cols-1 gap-4 py-4 sm:grid-cols-2 md:grid-cols-3 ">
         {projects.map((project, idx) => (
-          <MotionCard
+          <Link
             key={idx}
-            initial={{ opacity: 0, filter: "blur(10px)", y: 10 }}
-            whileInView={{ opacity: 1, filter: "blur(0px)", y: 0 }}
-            transition={{
-              duration: 0.3,
-              delay: idx * 0.1,
-              ease: "easeInOut",
-            }}
-            className="group flex flex-col overflow-hidden rounded-xl bg-card dark:bg-card shadow hover:shadow-md transition-shadow hover:scale-[1.01] duration-300 ease-in-out p-3"
+            href={project.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="no-underline block h-full"
           >
-            <div className="relative w-full overflow-hidden">
-              <Image
-                src={project.src}
-                alt={project.title}
-                width={400}
-                height={240}
-                className="h-auto w-full object-cover transition-transform duration-300 rounded-xl group-hover:scale-[1.01] ease-in-out"
-              />
-            </div>
+            <MotionCard
+              key={idx}
+              initial={{ opacity: 0, filter: "blur(10px)", y: 10 }}
+              whileInView={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+              transition={{
+                duration: 0.3,
+                delay: idx * 0.1,
+                ease: "easeInOut",
+              }}
+              className="group h-full flex flex-col overflow-hidden rounded-xl bg-card dark:bg-card shadow hover:shadow-md transition-shadow hover:scale-[1.01] duration-300 ease-in-out p-3"
+            >
+              <div className="relative w-full overflow-hidden">
+                <Image
+                  src={project.src}
+                  alt={project.title}
+                  width={400}
+                  height={240}
+                  className="h-auto w-full object-cover transition-transform duration-300 rounded-xl group-hover:scale-[1.01] ease-in-out"
+                />
+              </div>
 
-            <CardContent className="space-y-2 p-2">
-              <CardTitle className="text-lg text-foreground font-semibold leading-tight">
-                {project.title}
-              </CardTitle>
-              <p className="text-sm text-muted-foreground">
-                {project.description}
-              </p>
-            </CardContent>
-          </MotionCard>
+              <CardContent className="space-y-2 p-2">
+                <CardTitle className="text-lg text-foreground font-semibold leading-tight">
+                  {project.title}
+                </CardTitle>
+                <p className="text-sm text-muted-foreground">
+                  {project.description}
+                </p>
+              </CardContent>
+            </MotionCard>
+          </Link>
         ))}
       </div>
     </div>
--- constants/projects.ts
+++ constants/projects.ts
@@ -9,20 +9,20 @@ export const projects: Project[] = [
   {
     title: "Invoice-Cart",
     src: "/invoice-cart.png",
-    href: "#",
+    href: "https://invoice-cart.vercel.app",
     description: "A SaaS platform for creating and managing invoices.",
   },
   {
     title: "Ping-panda",
     src: "/ping-panda.png",
-    href: "#",
+    href: "https://ping-panda-rust.vercel.app",
     description:
       "A SaaS platform for sending and receiving notifications over discord.",
   },
   {
     title: "Record-scribe",
     src: "/record-scribe.png",
-    href: "#",
+    href: "https://recordscribe.vercel.app",
     description:
       "A website which translate and transcribe audio to different languages.",
   },
